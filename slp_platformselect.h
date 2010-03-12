6c021515ec56aec6fe7e905cba603f4a7db30b8d
Kyle Ambroff <ambroff@lindenlab.com>
2010-03-12 13:17:50 -0800

    Move platform specific slp_switch() implementations into platform directory.
    --HG--
    rename : switch_amd64_unix.h => platform/switch_amd64_unix.h
    rename : switch_arm32_gcc.h => platform/switch_arm32_gcc.h
    rename : switch_mips_unix.h => platform/switch_mips_unix.h
    rename : switch_ppc_macosx.h => platform/switch_ppc_macosx.h
    rename : switch_ppc_unix.h => platform/switch_ppc_unix.h
    rename : switch_s390_unix.h => platform/switch_s390_unix.h
    rename : switch_sparc_sun_gcc.h => platform/switch_sparc_sun_gcc.h
    rename : switch_x86_msvc.h => platform/switch_x86_msvc.h
    rename : switch_x86_unix.h => platform/switch_x86_unix.h

--- slp_platformselect.h
+++ slp_platformselect.h
@@ -3,23 +3,23 @@
  */
 
 #if   defined(MS_WIN32) && !defined(MS_WIN64) && defined(_M_IX86)
-#include "switch_x86_msvc.h" /* MS Visual Studio on X86 */
+#include "platform/switch_x86_msvc.h" /* MS Visual Studio on X86 */
 #elif defined(__GNUC__) && defined(__amd64__)
-#include "switch_amd64_unix.h" /* gcc on amd64 */
+#include "platform/switch_amd64_unix.h" /* gcc on amd64 */
 #elif defined(__GNUC__) && defined(__i386__)
-#include "switch_x86_unix.h" /* gcc on X86 */
+#include "platform/switch_x86_unix.h" /* gcc on X86 */
 #elif defined(__GNUC__) && defined(__PPC__) && defined(__linux__)
-#include "switch_ppc_unix.h" /* gcc on PowerPC */
+#include "platform/switch_ppc_unix.h" /* gcc on PowerPC */
 #elif defined(__GNUC__) && defined(__ppc__) && defined(__APPLE__)
-#include "switch_ppc_macosx.h" /* Apple MacOS X on PowerPC */
+#include "platform/switch_ppc_macosx.h" /* Apple MacOS X on PowerPC */
 #elif defined(__GNUC__) && defined(sparc) && defined(sun)
-#include "switch_sparc_sun_gcc.h" /* SunOS sparc with gcc */
+#include "platform/switch_sparc_sun_gcc.h" /* SunOS sparc with gcc */
 #elif defined(__GNUC__) && defined(__s390__) && defined(__linux__)
-#include "switch_s390_unix.h"	/* Linux/S390 */
+#include "platform/switch_s390_unix.h"	/* Linux/S390 */
 #elif defined(__GNUC__) && defined(__s390x__) && defined(__linux__)
-#include "switch_s390_unix.h"	/* Linux/S390 zSeries (identical) */
+#include "platform/switch_s390_unix.h"	/* Linux/S390 zSeries (identical) */
 #elif defined(__GNUC__) && defined(__arm__)
-#include "switch_arm32_gcc.h" /* gcc using arm32 */
+#include "platform/switch_arm32_gcc.h" /* gcc using arm32 */
 #elif defined(__GNUC__) && defined(__mips__) && defined(__linux__)
-#include "switch_mips_unix.h" /* Linux/MIPS */
+#include "platform/switch_mips_unix.h" /* Linux/MIPS */
 #endif
