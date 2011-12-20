e483f28bfbbd7e6ef994401e0461bc31c59ea25b
Ralf Schmitt <ralf@systemexit.de>
2011-12-20 15:49:53 +0100

    show if running on a 32bit/64bit platform in run-tests.py
    that's useful e.g. on OS X with it's fat binaries. Use the arch
    command to switch architecture on OS X:
    
    arch -x86_64 python2.6 ./run-tests.py -q
    arch -i386 python2.6 ./run-tests.py -q

--- run-tests.py
+++ run-tests.py
@@ -7,6 +7,27 @@ build = True
 verbosity = 2
 
 
+def bits():
+    """determine if running on a 32 bit or 64 bit platform
+    just looking at sys.maxint isn't enough
+    x64 windows has sys.maxint == (1<<31) - 1
+    """
+
+    if sys.version_info[:2] < (2, 5):
+        if sys.maxint == (1 << 31) - 1:
+            return 32
+        else:
+            return 64
+
+    class X(object):
+        def __len__(self):
+            return 1 << 31
+    try:
+        len(X())
+        return 64
+    except OverflowError:
+        return 32
+
 # -- parse options
 try:
     opts, args = getopt.getopt(sys.argv[1:], "nq")
@@ -41,8 +62,8 @@ import greenlet
 if not build:
     sys.path[:] = oldpath
 
-sys.stdout.write("python %s using greenlet %s from %s\n" %
-                 (sys.version.split()[0], greenlet.__version__, greenlet.__file__))
+sys.stdout.write("python %s (%s bit) using greenlet %s from %s\n" %
+                 (sys.version.split()[0], bits(), greenlet.__version__, greenlet.__file__))
 
 
 # -- run tests
