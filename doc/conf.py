bf21334824e2111f7c107cda973068e38444f697
Ralf Schmitt <ralf@systemexit.de>
2011-12-27 23:52:38 +0100

    make the documentation compatible with readthedocs.org
    It should be available at http://greenlet.readthedocs.org and should
    automatically be updated when new revisions are pushed to the github
    repo.

--- doc/conf.py
+++ doc/conf.py
@@ -37,7 +37,7 @@ source_suffix = '.txt'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'greenlet'
+master_doc = 'index'
 
 # General information about the project.
 project = u'greenlet'
