8ec0bc838be654809e41c47ae269ba6e8afe08b8
Alexey Borzenkov <snaury@gmail.com>
2012-01-28 15:47:56 +0400

    Save floating point registers on arm32

--- platform/switch_arm32_gcc.h
+++ platform/switch_arm32_gcc.h
@@ -25,7 +25,17 @@
 
 #ifdef SLP_EVAL
 #define STACK_MAGIC 0
-#define REGS_TO_SAVE "r4", "r5", "r6", "r7", "r8", "r9", "lr"
+#define REGS_TO_SAVE_GENERAL "r4", "r5", "r6", "r7", "r8", "r9", "lr"
+#if defined(__VFP_FP__)
+#define REGS_TO_SAVE REGS_TO_SAVE_GENERAL, "d8", "d9", "d10", "d11", \
+                                           "d12", "d13", "d14", "d15"
+#elif defined(__MAVERICK__)
+#define REGS_TO_SAVE REGS_TO_SAVE_GENERAL, "mvf4", "mvf5", "mvf6", "mvf7", \
+                                           "mvf8", "mvf9", "mvf10", "mvf11", \
+                                           "mvf12", "mvf13", "mvf14", "mvf15"
+#else
+#define REGS_TO_SAVE REGS_TO_SAVE_GENERAL, "f4", "f5", "f6", "f7"
+#endif
 
 static int
 slp_switch(void)
