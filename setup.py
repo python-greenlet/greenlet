392ed34fc7416654002ac54a6e096332d03ed975
Alexey Borzenkov <snaury@gmail.com>
2014-08-07 23:49:16 +0400

    Increment version to 0.4.3

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ from my_build_ext import build_ext
 
 setup(
     name="greenlet",
-    version='0.4.2',
+    version='0.4.3',
     description='Lightweight in-process concurrent programming',
     long_description=readfile("README.rst"),
     maintainer="Alexey Borzenkov",
