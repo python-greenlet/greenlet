51260742e3e58280e1ef860a888cfd1c8a96ad79
Alexey Borzenkov <snaury@gmail.com>
2014-08-08 01:41:44 +0400

    Add Python 3.3 and 3.4 to classifiers

--- setup.py
+++ setup.py
@@ -86,6 +86,8 @@ setup(
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules'],
     **setuptools_args)
