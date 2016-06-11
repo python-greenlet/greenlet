04303e5b8d4b0f7a4e9c9d02e3905009e55cbd45
Alexey Borzenkov <snaury@gmail.com>
2016-06-11 20:25:44 +0300

    Prepare for 0.4.10 release

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ from my_build_ext import build_ext
 
 setup(
     name="greenlet",
-    version='0.4.10.dev',
+    version='0.4.10',
     description='Lightweight in-process concurrent programming',
     long_description=readfile("README.rst"),
     maintainer="Alexey Borzenkov",
