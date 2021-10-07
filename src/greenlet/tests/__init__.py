831ed82228bd336c5ac0733f9dc028f239c5fcf1
Jason Madden <jamadden@gmail.com>
2021-10-07 13:01:46 -0500

    Try harder to wait for active threads to die.

--- src/greenlet/tests/__init__.py
+++ src/greenlet/tests/__init__.py
@@ -9,21 +9,28 @@ from __future__ import print_function
 
 from gc import collect
 from gc import get_objects
+from threading import active_count as active_thread_count
 
 from greenlet import greenlet as RawGreenlet
 from greenlet import getcurrent
 
 class CleanupMixin(object):
 
-    def wait_for_pending_cleanups(self):
+    def wait_for_pending_cleanups(self, initial_active_threads=1):
         from time import sleep
         from time import time
         from greenlet._greenlet import get_pending_cleanup_count
         sleep_time = 0.001
+        # NOTE: This is racy! A Python-level thread object may be dead
+        # and gone, but the C thread may not yet have fired its
+        # destructors and added to the queue. There's no particular
+        # way to know that's about to happen. We try to watch the
+        # Python threads to make sure they, at least, have gone away.
+
         # Always sleep at least once to let other threads run
         sleep(sleep_time)
         quit_after = time() + 10
-        while get_pending_cleanup_count():
+        while get_pending_cleanup_count() or active_thread_count() > initial_active_threads:
             sleep(sleep_time)
             if time() > quit_after:
                 break
@@ -61,7 +68,7 @@ class CleanupMixin(object):
         # Ensure the main greenlet exists, otherwise the first test
         # gets a false positive leak
         getcurrent()
-        self.wait_for_pending_cleanups()
+        self.wait_for_pending_cleanups(active_thread_count())
         self.greenlets_before_test = self.count_greenlets()
 
     def tearDown(self):
