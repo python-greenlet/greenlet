c7f8e7e153fbb7eb0cee9f5b8b5a38fc8c7f5e45
Jason Madden <jamadden@gmail.com>
2022-01-24 06:54:37 -0600

    Port #285 from os.fork to multiprocessing so it can run on Windows.

--- src/greenlet/tests/test_cpp.py
+++ src/greenlet/tests/test_cpp.py
@@ -1,13 +1,22 @@
 from __future__ import print_function
 from __future__ import absolute_import
 
-import unittest
-import os
 import signal
+from multiprocessing import Process
 
 import greenlet
 from . import _test_extension_cpp
 from . import TestCase
+
+def run_unhandled_exception_in_greenlet_aborts():
+    # This is used in multiprocessing.Process and must be picklable
+    # so it needs to be global.
+    def _():
+        _test_extension_cpp.test_exception_switch_and_do_in_g2(
+            _test_extension_cpp.test_exception_throw
+        )
+    g1 = greenlet.greenlet(_)
+    g1.switch()
 
 class CPPTests(TestCase):
     def test_exception_switch(self):
@@ -19,44 +28,20 @@ class CPPTests(TestCase):
         for i, g in enumerate(greenlets):
             self.assertEqual(g.switch(), i)
 
-    @unittest.skipUnless(hasattr(os, 'fork'),
-        "test should abort when run -> need to run it in separate process")
-    def test_exception_switch_and_throw(self):
-        # procrun runs f in separate process
-        def procrun(f): # -> (ret, sig, coredumped)
-            pid = os.fork()
-
-            # child
-            if pid == 0:
-                f()
-                os.exit(0)
-
-            # parent
-            _, st = os.waitpid(pid, 0)
-            ret = st >> 8
-            sig = st & 0x7f
-            coredumped = ((st & 0x80) != 0)
-            return (ret, sig, coredumped)
-
-
+    def test_unhandled_exception_aborts(self):
         # verify that plain unhandled throw aborts
-        # (unhandled throw -> std::terminate -> abort)
-        ret, sig, _ = procrun(_test_extension_cpp.test_exception_throw)
-        if not (ret == 0 and sig == signal.SIGABRT):
-            self.fail("unhandled throw -> ret=%d sig=%d  ; expected 0/SIGABRT" % (ret, sig))
 
+        # TODO: On some versions of Python with some settings, this
+        # spews a lot of garbage to stderr. It would be nice to capture and ignore that.
+        p = Process(target=_test_extension_cpp.test_exception_throw)
+        p.start()
+        p.join(10)
+        self.assertEqual(p.exitcode, - signal.SIGABRT)
 
+    def test_unhandled_exception_in_greenlet_aborts(self):
         # verify that unhandled throw called in greenlet aborts too
-        # (does not segfaults nor is handled by try/catch on preceeding greenlet C stack)
-        def _():
-            def _():
-                _test_extension_cpp.test_exception_switch_and_do_in_g2(
-                    _test_extension_cpp.test_exception_throw
-                )
-            g1 = greenlet.greenlet(_)
-            g1.switch()
-
-        ret, sig, core = procrun(_)
-        if not (ret == 0 and sig == signal.SIGABRT):
-            self.fail("failed with ret=%d sig=%d%s" %
-                      (ret, sig, " (core dumped)" if core else ""))
+        # TODO: See test_unhandled_exception_aborts
+        p = Process(target=run_unhandled_exception_in_greenlet_aborts)
+        p.start()
+        p.join(10)
+        self.assertEqual(p.exitcode, - signal.SIGABRT)
