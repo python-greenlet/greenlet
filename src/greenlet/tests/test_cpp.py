b342f937230c64f775bcdb3c2fbe1eab1273ed1e
Jason Madden <jamadden@gmail.com>
2022-01-24 07:10:01 -0600

    Compensate for windows abort() terminating with 3 rather than -signal.SIGABRT

--- src/greenlet/tests/test_cpp.py
+++ src/greenlet/tests/test_cpp.py
@@ -28,20 +28,38 @@ class CPPTests(TestCase):
         for i, g in enumerate(greenlets):
             self.assertEqual(g.switch(), i)
 
-    def test_unhandled_exception_aborts(self):
-        # verify that plain unhandled throw aborts
-
+    def _do_test_unhandled_exception(self, target):
         # TODO: On some versions of Python with some settings, this
         # spews a lot of garbage to stderr. It would be nice to capture and ignore that.
-        p = Process(target=_test_extension_cpp.test_exception_throw)
+        import sys
+        WIN = sys.platform.startswith("win")
+
+        p = Process(target=target)
         p.start()
         p.join(10)
-        self.assertEqual(p.exitcode, - signal.SIGABRT)
+        # The child should be aborted in an unusual way. On POSIX
+        # platforms, this is done with abort() and signal.SIGABRT,
+        # which is reflected in a negative return value; however, on
+        # Windows, even though we observe the child print "Fatal
+        # Python error: Aborted" and in older versions of the C
+        # runtime "This application has requested the Runtime to
+        # terminate it in an unusual way," it always has an exit code
+        # of 3. This is interesting because 3 is the error code for
+        # ERROR_PATH_NOT_FOUND; BUT: the C runtime abort() function
+        # also uses this code.
+        #
+        # See
+        # https://devblogs.microsoft.com/oldnewthing/20110519-00/?p=10623
+        # and
+        # https://docs.microsoft.com/en-us/previous-versions/k089yyh0(v=vs.140)?redirectedfrom=MSDN
+        expected_exit = -signal.SIGABRT if not WIN else 3
+        self.assertEqual(p.exitcode, expected_exit)
+
+    def test_unhandled_exception_aborts(self):
+        # verify that plain unhandled throw aborts
+        self._do_test_unhandled_exception(_test_extension_cpp.test_exception_throw)
+
 
     def test_unhandled_exception_in_greenlet_aborts(self):
         # verify that unhandled throw called in greenlet aborts too
-        # TODO: See test_unhandled_exception_aborts
-        p = Process(target=run_unhandled_exception_in_greenlet_aborts)
-        p.start()
-        p.join(10)
-        self.assertEqual(p.exitcode, - signal.SIGABRT)
+        self._do_test_unhandled_exception(run_unhandled_exception_in_greenlet_aborts)
