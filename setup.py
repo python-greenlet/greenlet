6fba0cddc1f7fc02b7c177793604ed438aac4395
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-11-23 09:12:26 -0600

    Declare support for Python 3.12 in trove classifiers

--- setup.py
+++ setup.py
@@ -240,6 +240,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
