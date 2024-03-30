8d7115ce2c8a2568cf4385fb91d5683b6b9de3be
Havard Eidnes <he@NetBSD.org>
2024-03-30 19:09:00 +0000

    slp_platformselect.h: add recognition of NetBSD/powerpc.

--- src/greenlet/slp_platformselect.h
+++ src/greenlet/slp_platformselect.h
@@ -27,6 +27,8 @@ extern "C" {
 # include "platform/switch_ppc64_aix.h" /* gcc on AIX/PowerPC 64-bit */
 #elif defined(__GNUC__) && defined(_ARCH_PPC) && defined(_AIX)
 # include "platform/switch_ppc_aix.h" /* gcc on AIX/PowerPC */
+#elif defined(__GNUC__) && defined(__powerpc__) && defined(__NetBSD__)
+#include "platform/switch_ppc_unix.h" /* gcc on NetBSD/powerpc */
 #elif defined(__GNUC__) && defined(sparc)
 # include "platform/switch_sparc_sun_gcc.h" /* SunOS sparc with gcc */
 #elif defined(__SUNPRO_C) && defined(sparc) && defined(sun)
